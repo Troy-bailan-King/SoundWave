23f1e7d859885ae2e047593812157049796b78ee
Troy-bailan-King <97757033+Troy-bailan-King@users.noreply.github.com>
2023-06-18 23:02:19 -0400

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,48 @@ import { SignInButton, SignOutButton, useUser,UserButton } from "@clerk/nextjs";
 import { User } from "@clerk/nextjs/dist/types/server";
 import { type NextPage } from "next";
 import Head from "next/head";
-import { api } from "~/utils/api";
+import { api, RouterOutputs } from "~/utils/api";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const user = useUser();
   if(!user.isSignedIn) return null;
   return (
-    <UserButton />
+    <div className="flex w-full gap-3">
+      <><UserButton />
+    <input className="grow bg-transparent outline-none" placeholder="What's on your mind?"></input></>
+    </div>
+    
   );
-}
+};
+
+type PostWithUser = RouterOutputs["posts"]["getAll"][number]
+
+const PostView = (props: PostWithUser) => {
+  const {post, author} = props;
+  return(
+    <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
+      <img src={author.profileImageUrl} className="h-14 w-14 rounded-full"/>
+            <div className="flex flex-col">
+              <div className="flex gap-1 text-slate-300">
+                <span>{`@${author.username!} `}</span>
+                <span className="font-thin">{`·${dayjs(post.createdAt).fromNow()}`}</span> 
+              </div>
+              <span>{post.content}</span>
+            </div>
+            
+          </div>
+  );
+} 
 
 
 
 const Home: NextPage = () => {
   const user = useUser();
-
+  
   const{data, isLoading} = api.posts.getAll.useQuery();
 
   if(isLoading) return (<div>Loading...</div>);
@@ -36,12 +63,11 @@ const Home: NextPage = () => {
           {!user.isSignedIn&&<div className="flex justify-center"><SignInButton /></div>}
           {!!user.isSignedIn&&<CreatePostWizard />}
         </div>
-        <div className="flex flex-col">
-          {[...data, ...data]?.map((post) => (<div key={post.id} className="border-b border-slate-400 p-8">
-            {post.content  }
-            </div>
-          ))}
-        </div>
+        <div className="flex grow flex-col overflow-y-scroll">
+      {[...data, ...data].map((fullPost) => (
+        <PostView {...fullPost} key={fullPost.post.id} />
+      ))}
+    </div>
         </div>
       </main>
     </> 
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,9 +1,49 @@
+import { auth, clerkClient } from '@clerk/nextjs';
 import {z} from 'zod';
+import { User } from "@clerk/nextjs/dist/types/server";
 
 import { createTRPCRouter,publicProcedure } from '~/server/api/trpc';
+import { TRPCError } from "@trpc/server";
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.profileImageUrl,
+    externalUsername: user.externalAccounts.find((externalAccount) => externalAccount.provider === "oauth_github")?.username || null
+  };
+};
 
 export const postsRouter = createTRPCRouter({
-    getAll: publicProcedure.query(({ctx})=>{
-        return ctx.prisma.post.findMany();
+    getAll: publicProcedure.query(async ({ctx})=>{
+        const posts = await ctx.prisma.post.findMany({
+            take: 100, 
+        });
+
+        const users = (
+            await clerkClient.users.getUserList({
+            userId: posts.map((post)=>post.authorId),
+            limit: 100,
+        })).map(filterUserForClient);
+
+        
+        console.log(users);
+        return posts.map((post)=>{
+            const author = users.find((user) => user.id === post.authorId);
+            console.log(author);
+            if(!author)
+                throw new TRPCError({code:"INTERNAL_SERVER_ERROR",
+                message:"Author not found"});
+
+            if(!author.username)
+                throw new TRPCError({code:"INTERNAL_SERVER_ERROR",
+                message:"name not found"});
+
+            return{
+                post,
+                author
+            }
+            
+    });
     }),
 });
